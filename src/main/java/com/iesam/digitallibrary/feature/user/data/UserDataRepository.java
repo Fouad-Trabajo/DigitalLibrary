abdccace656836934c292ca4f1d6a00ca940d25c
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-04-11 16:27:20 +0200

    feat: Dar de alta a un usuario
    Se crea la estructura inicial de clases

---
+++ src/main/java/com/iesam/digitallibrary/feature/user/data/UserDataRepository.java
@@ -0,0 +1,8 @@
+package com.iesam.digitallibrary.feature.user.data;
+
+import com.iesam.digitallibrary.feature.user.domain.UserRepository;
+
+/**
+ * Esta es la clase que me permite gestionar el acceso a fuentes de datos*/
+public class UserDataRepository implements UserRepository {
+}
