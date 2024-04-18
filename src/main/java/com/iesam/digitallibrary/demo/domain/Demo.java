ea726baad5e6a774d0576a7a649fe59a351adefa
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-04-18 15:36:07 +0200

    Añadido diagrama uml y clase demo

---
+++ src/main/java/com/iesam/digitallibrary/demo/domain/Demo.java
@@ -0,0 +1,14 @@
+package com.iesam.digitallibrary.demo.domain;
+
+public class Demo {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
